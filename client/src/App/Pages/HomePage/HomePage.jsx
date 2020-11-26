98ad8c282c9279546eacb4028e8416e9b41dfe11
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-26 11:51:06 +0700

    Fetching the data from the backend server

--- client/src/App/Pages/HomePage/HomePage.jsx
+++ client/src/App/Pages/HomePage/HomePage.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import ProductCard from '../../Components/ProductCard/ProductCard'
-import products from './products'
+import React, { useEffect, useState } from 'react'
+ import ProductCard from '../../Components/ProductCard/ProductCard'
+ import axios from 'axios'
+
 
 import './style.css'
 
 export default function HomePage() {
+
+    const [products, setProducts] = useState([])
+    
+    useEffect(()=>{
+      
+      const fetchData = async()=>{
+        try {
+            const {data} = await axios.get(`/api/products`)
+            setProducts(data)
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    fetchData();
+    },[])
+
+
     return (
         <div className='home_page'  >
           {
               products.map(product =>(
-                  <div >
                   <ProductCard key={product.name} product={product}  />
-                </div>
               )
             )
           }
--- client/src/App/Pages/ProductDetailedPage/ProductDetailedPage.jsx
+++ client/src/App/Pages/ProductDetailedPage/ProductDetailedPage.jsx
@@ -1,14 +1,24 @@
 import { Container } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import DetialsHeader from '../../Components/ProductDetials/DetialsHeader'
 import ProductDescription from '../../Components/ProductDetials/ProductDescription'
-import products from '../HomePage/products'
+// import products from '../HomePage/products'
 
 export default function ProductDetailedPage() {
-    const {id} = useParams()
-    const product = products.find(p =>
-        p.id === id)
+    const [product, setProduct] = useState([])
+
+    const { id } = useParams()
+    
+    useEffect(()=>{
+      const fetchData = async()=>{
+          fetch(`/api/products/${id}`)
+          .then(response => response.json())
+          .then(data => setProduct(data))
+      }
+      fetchData();
+  },[id])
+
     return (
         <div style={{marginTop: 200}}>
             <Container>
