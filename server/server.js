240a101a9efe36b0e25f6e3f0b2ba060d3f59763
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-27 14:39:24 +0700

    Handlling Error

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import productRouter from './Routes/productRoutes.js'
 import colors from 'colors'
 import connectDB from './config/db.js';
 
+import { notFound, errorHandler } from './middleware/errorHandler.js'
+
 
 dotenv.config()
 const app = express();
@@ -17,5 +19,8 @@ app.use('/api/products', productRouter)
 const PORT = process.env.PORT|| 5000;
 
 connectDB()
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`app is runing in ${process.env.NODE_ENV} on port ${PORT}` .blue.underline ))
