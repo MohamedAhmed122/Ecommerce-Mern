e578f61d7cd23d2a9b22617a31a5ba3192d870ce
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-27 14:04:51 +0700

    Setting the Product route and fetch data from the db to the backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from'express';
 import dotenv from'dotenv'
-import products from'./data/products.js';
+import productRouter from './Routes/productRoutes.js'
 import colors from 'colors'
 import connectDB from './config/db.js';
 
@@ -12,14 +12,8 @@ app.get('/',(req, res)=>{
     res.send('Api is Running .... . .. . .')
 })
 
-app.get('/api/products', (req,res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRouter)
 
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 const PORT = process.env.PORT|| 5000;
 
 connectDB()
